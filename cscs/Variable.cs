eed7bcfab4d669f765165d98880976beeccbd262
Vassili Kaplan <vassilik@gmail.com>
2018-09-02 15:38:33 +0200

    some comments and use cases added

--- cscs/Variable.cs
+++ cscs/Variable.cs
@@ -665,10 +665,18 @@ namespace SplitAndMerge
         Dictionary<string, Variable> m_propertyMap = new Dictionary<string, Variable>();
     }
 
+    // A Variable supporting "dot-notation" must have an object implementing this interface.
     public interface ScriptObject
     {
+        // SetProperty is triggered by the following scripting call: "a.name = value;"
         Variable SetProperty(string name, Variable value);
+
+        // GetProperty is triggered by the following scripting call: "x = a.name;"
+        // If args are not null, it is triggered by a function call: "y = a.name(arg1, arg2, ...);"
         Variable GetProperty(string name, List<Variable> args = null);
+
+        // Returns all of the properties that this object implements. Only these properties will be processed
+        // by SetProperty() and GetProperty() methods above.
         List<string> GetProperties();
     }
 }
